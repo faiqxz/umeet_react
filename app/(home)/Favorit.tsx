1b6e381fc417a82fd65263b0a798f9143a16c07a
Fahmousss <09031382227177@student.unsri.ac.id>
2024-12-02 23:58:39 +0700

    Fix bug

--- app/(home)/Favorit.tsx
+++ app/(home)/Favorit.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
-import { supabase } from '@/lib/supabase';
-import EventCard from '@/components/EventCard';
-import { EventList } from '@/components/EventList';
-
-
+import React from "react";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+import { supabase } from "@/lib/supabase";
+import EventCard from "@/components/EventCard";
+import { EventList } from "@/components/EventList";
+import useEventData from "@/hooks/useEventData";
 
 const FavoritScreen = () => {
-    return (
-      <View style={styles.container}>
-        <EventList />
-      </View>
-    );
-  };
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#f9f9f9",
-    },
-  });
-  
-  export default FavoritScreen;
+  const { eventData } = useEventData(null);
+  return (
+    <View>
+      <EventList
+        event={eventData}
+        isHorizontal={false}
+        footerComponent={null}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f9f9f9",
+  },
+});
+
+export default FavoritScreen;
--- app/(home)/Terdaftar.tsx
+++ app/(home)/Terdaftar.tsx
@@ -3,56 +3,58 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
-import EventCard from '@/components/EventCard';
-import { EventList } from '@/components/EventList';
+import EventCard from "@/components/EventCard";
+import { EventList } from "@/components/EventList";
+import useEventData from "@/hooks/useEventData";
 
-export default function TerdaftarScreen () {
-  
-  
+export default function TerdaftarScreen() {
+  const { eventData } = useEventData(null);
   return (
     <View>
-      <EventList/>
+      <EventList
+        event={eventData}
+        isHorizontal={false}
+        footerComponent={null}
+      />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   card: {
     margin: 10,
     padding: 10,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 2 },
   },
   image: {
-    width: '100%',
+    width: "100%",
     height: 150,
     borderRadius: 10,
   },
   title: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginVertical: 5,
   },
   organizer: {
     fontSize: 14,
-    color: '#555',
+    color: "#555",
   },
   category: {
     fontSize: 14,
-    color: '#555',
+    color: "#555",
   },
   date: {
     fontSize: 14,
-    color: '#555',
+    color: "#555",
   },
   description: {
     fontSize: 12,
-    color: '#777',
+    color: "#777",
     marginVertical: 5,
   },
 });
-
-
